0a9046a491f3c47f30641f6819b368d6d1ffc300
ChapelleSophie <43351595+ChapelleSophie@users.noreply.github.com>
2018-09-26 21:07:03 +0200

    Zadanie dodatkowe
    zad.dodatkowe

---
+++ dodatkowe.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "zad 1." << endl ;
+    int a,b ;
+    cin >> a >> b ;
+    cout << (a*a) + (b*b) ;
+
+    int a,b;
+    cin >> a >> b;
+    cout << (a+b)*(a+b);
+
+    int a,b;
+    cin >> a >> b;
+    cout << pow(a-b,3);
+
+
+
+    int a,b,c;
+    cin >> a >> b >> c ;
+    cout <<( a*b*c);
+
+
+   int a,b;
+   cin >> a >> b ;
+   cout << 2*(a+b)/5 << (a-b)*0,4 ;
+
+
+   int brutto;
+   cin >> brutto ;
+   cout << brutto/1,23;
+
+
+   int a,b;
+   cin >> a >> b ;
+   cout << a%b ;
+
+}
